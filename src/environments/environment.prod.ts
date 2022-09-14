e23b66cb3cd71f7a4d3899d1844a6b7d98371525
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-09-13 21:05:35 -0400

    massive structuring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-demo-1af9a',
+    appId: '1:846707372478:web:2da1ef6a8a7446298e89ef',
+    storageBucket: 'fir-demo-1af9a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCv9p50W0RZrqDIwusKU92MgYza3MbZKV8',
+    authDomain: 'fir-demo-1af9a.firebaseapp.com',
+    messagingSenderId: '846707372478',
+    measurementId: 'G-R1SR4JDW77',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fir-demo-1af9a',
+    appId: '1:846707372478:web:2da1ef6a8a7446298e89ef',
+    storageBucket: 'fir-demo-1af9a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCv9p50W0RZrqDIwusKU92MgYza3MbZKV8',
+    authDomain: 'fir-demo-1af9a.firebaseapp.com',
+    messagingSenderId: '846707372478',
+    measurementId: 'G-R1SR4JDW77',
+  },
   production: false
 };
 
